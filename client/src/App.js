9df96eafdec4706e471fb0e30e709589dbe58b15
ty <sycork@gamil.com>
2023-01-09 16:17:22 +0900

    fe_init

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">헤더입니다.</header>
+      <main>안녕하세요 메인입니다.</main>
+      <footer>푸터입니다.</footer>
+    </div>
+  );
+}
+
+export default App;
