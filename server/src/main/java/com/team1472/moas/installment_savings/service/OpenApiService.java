c24969fc6be95c4911a153d5af6f533756b38ebd
gahyeonn <parka0017@gmail.com>
2023-01-10 17:44:04 +0900

    [BE feat] open API 적금 조회 정보 DB 저장 기능 구현

---
+++ server/src/main/java/com/team1472/moas/installment_savings/service/OpenApiService.java
@@ -0,0 +1,92 @@
+package com.team1472.moas.installment_savings.service;
+
+import com.google.gson.*;
+import com.team1472.moas.installment_savings.entity.InstallmentSavings;
+import com.team1472.moas.installment_savings.entity.InterestRate;
+import com.team1472.moas.installment_savings.repository.InstallmentSavingsRepository;
+import com.team1472.moas.installment_savings.repository.InterestRateRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OpenApiService {
+    private final InstallmentSavingsRepository installmentSavingsRepository;
+    private final InterestRateRepository interestRateRepository;
+
+    //금융감독원 적금 정보 파싱 후 DB 저장
+    public int getInstallmentSavingsInfo(String jsonData) {
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+
+        JsonElement element = JsonParser.parseString(jsonData);
+        JsonObject object = element.getAsJsonObject();
+
+        JsonObject result = object.getAsJsonObject("result");
+
+        if (!checkSuccessfulApiCall(result)) { //에러 발생 여부 확인
+            return 0;
+        }
+
+        int totalPageNum = result.get("max_page_no").getAsInt();
+
+        JsonArray baseList = result.get("baseList").getAsJsonArray(); //적금 기본 정보 리스트
+        List<InstallmentSavings> savingsList = parseInstallmentSavingsInfo(baseList, gson);
+        installmentSavingsRepository.saveAll(savingsList);
+
+        JsonArray optionList = result.get("optionList").getAsJsonArray(); //적금 이자율에 대한 정보 리스트
+        List<InterestRate> interestRateList = parseInterestRateInfo(optionList, gson);
+        interestRateRepository.saveAll(interestRateList);
+
+
+        return totalPageNum;
+    }
+
+    //에러 발생 여부 확인
+    private boolean checkSuccessfulApiCall(JsonObject response) {
+        String errorCode = response.get("err_cd").getAsString();
+
+        if (!errorCode.equals("000")) {
+            log.error("적금 조회 Open API 호출 실패 [응답 코드: " + errorCode + " 응답 메시지: " + response.get("err_msg") + "]");
+            return false;
+        }
+
+        return true;
+    }
+
+    //적금 상품에 대한 기본 정보 파싱
+    private List<InstallmentSavings> parseInstallmentSavingsInfo(JsonArray baseList, Gson gson) {
+        InstallmentSavings[] installmentSavings = gson.fromJson(baseList, InstallmentSavings[].class);
+
+        return Arrays.asList(installmentSavings);
+    }
+
+    //적금 상품의 이자율 정보 파싱
+    private List<InterestRate> parseInterestRateInfo(JsonArray optionList, Gson gson) {
+        List<InterestRate> interestRateList = new ArrayList<>();
+
+        for (int i = 0; i < optionList.size(); i++) {
+            JsonObject data = (JsonObject) optionList.get(i);
+
+            InterestRate interestRate = gson.fromJson(data, InterestRate.class);
+            Optional<InstallmentSavings> optSavings = installmentSavingsRepository.findByfinPrdtCd(interestRate.getFinPrdtCd());
+
+            optSavings.ifPresent(savings -> {
+                interestRate.addInstallmentSavings(savings);
+                interestRateList.add(interestRate);
+            });
+        }
+
+        return interestRateList;
+    }
+}
