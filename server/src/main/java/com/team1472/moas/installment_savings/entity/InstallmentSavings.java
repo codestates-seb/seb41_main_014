0aea656dcdaf55f5223d71440bf3527c463c7b44
GaHyeon <parka0017@gmail.com>
2023-01-11 10:32:24 +0900

    [BE feat] Open API 적금 조회 정보 DB 자동 저장 기능 구현 (#13)
    * [BE style] 적금 관련 entity 변수명 camel case로 수정
    
    * [BE feat] InterestRate entity 관계 추가 메서드 구현
    
    * [BE feat] open API 적금 조회 정보 DB 저장 기능 구현
    
    * [BE feat] 애플리케이션 실행 시 자동 적금 정보 DB 저장 기능 구현
    
    * [BE feat] 매일 새벽 4시 적금 정보 자동 업데이트 기능 구현

--- server/src/main/java/com/team1472/moas/installment_savings/entity/InstallmentSavings.java
+++ server/src/main/java/com/team1472/moas/installment_savings/entity/InstallmentSavings.java
@@ -1,7 +1,7 @@
 package com.team1472.moas.installment_savings.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Builder
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class InstallmentSavings {
@@ -17,31 +17,31 @@ public class InstallmentSavings {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private long fin_co_no; //금융회사 코드
+    private String finCoNo; //금융회사 코드
 
-    private String kor_co_nm; //금융회사 명
+    private String korCoNm; //금융회사 명
 
-    private long fin_prdt_cd; //금융 상품 코드
+    private String finPrdtCd; //금융 상품 코드
 
-    private String fin_prdt_nm; //금융상품명
+    private String finPrdtNm; //금융상품명
 
-    private String join_way; //가입 방법
-
-    @Column(columnDefinition = "TEXT")
-    private String mtrt_int; //만기 후 이자율
+    private String joinWay; //가입 방법
 
     @Column(columnDefinition = "TEXT")
-    private String spcl_cnd; //우대 조건
-
-    private int join_deny; //가입제한
+    private String mtrtInt; //만기 후 이자율
 
     @Column(columnDefinition = "TEXT")
-    private String join_member; //가입 대상
+    private String spclCnd; //우대 조건
+
+    private String joinDeny; //가입제한
 
     @Column(columnDefinition = "TEXT")
-    private String etc_note; //기타 유의사항
+    private String joinMember; //가입 대상
 
-    private int max_limit; //최고 한도
+    @Column(columnDefinition = "TEXT")
+    private String etcNote; //기타 유의사항
+
+    private int maxLimit; //최고 한도
 
     @OneToMany(mappedBy = "installmentSavings", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<InterestRate> interestRates = new ArrayList<>();
