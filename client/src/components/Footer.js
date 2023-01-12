7fedd987a94c8ad81a56cd88df5ff72508239d1e
홍동화 <hhhhong44@gmail.com>
2023-01-12 17:15:42 +0900

    [FE] Footer 크기변경, 하단배치

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,12 @@
 import styled from '@emotion/styled';
 
-const StyeldFooter = styled.footer``;
+const StyeldFooter = styled.footer`
+  width: 100%;
+  height: 200px;
+  background-color: #b1b2ff;
+  position: absolute;
+  bottom: 0;
+`;
 
 const Footer = () => {
   return <StyeldFooter>푸터푸터푸터</StyeldFooter>;
