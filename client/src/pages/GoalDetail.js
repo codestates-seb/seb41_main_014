cdadbf87815653a26028d0a3a24c65685b24d49f
홍동화 <hhhhong44@gmail.com>
2023-01-12 17:41:03 +0900

    [FE Design] 상세 목표 틀 설정

--- client/src/pages/GoalDetail.js
+++ client/src/pages/GoalDetail.js
@@ -1,5 +1,47 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import { ROUTE_PATH_GOAL_LIST } from '../store/routerStore';
+
 const GoalDetail = () => {
-  return <>목표상세</>;
+  return (
+    <GDetailPage>
+      <h3 style={{ marginTop: '30px' }}>💜 상세 위시 정보 💜</h3>
+      <GDetail>
+        <div>
+          <button className="BackButton">
+            <Link to={ROUTE_PATH_GOAL_LIST} style={{ textDecoration: 'none' }}>
+              ⬅️
+            </Link>
+          </button>
+        </div>
+      </GDetail>
+    </GDetailPage>
+  );
 };
 
 export default GoalDetail;
+
+const GDetailPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GDetail = styled.div`
+  width: 568px;
+  height: 649px;
+  border-radius: 6px;
+  background-color: #eef1ff;
+  margin: 30px 0 50px 0;
+  .BackButton {
+    border: 0;
+    border-radius: 50%;
+    width: 48px;
+    height: 48px;
+    background-color: #b1b2ff;
+    margin: 5px 5px 5px 5px;
+    font-size: large;
+    color: #626262;
+  }
+`;
