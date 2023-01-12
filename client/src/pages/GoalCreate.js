c3a606b8c2fe40bbefb6da797454b4ca3484c342
ty <sycork@gamil.com>
2023-01-12 15:06:25 +0900

    [FE Feat] 네이버검색 api 완료 및 ghpages로 배포

--- client/src/pages/GoalCreate.js
+++ client/src/pages/GoalCreate.js
@@ -1,5 +1,43 @@
+import { Button, Container, Input } from '@mui/material';
+import { useState } from 'react';
+import axios from 'axios';
+
 const GoalCreate = () => {
-  return <>목표생성</>;
+  const [search, setSearch] = useState('');
+  const handleSearchInput = (e) => {
+    setSearch(e.target.value);
+  };
+  const handleSearch = () => {
+    console.log(search);
+    console.log(process.env.REACT_APP_NAVER_SEARCH_ENDPOINT);
+    axios({
+      method: 'get',
+      url: process.env.REACT_APP_NAVER_SEARCH_ENDPOINT,
+      headers: {
+        'X-Naver-Client-Id': process.env.REACT_APP_NAVER_API_CLIENT_ID,
+        'X-Naver-Client-Secret': process.env.REACT_APP_NAVER_API_CLIENT_SECRET,
+      },
+      params: {
+        query: search,
+      },
+      proxy: {
+        protocol: 'https',
+        host: 'openapi.naver.com',
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <Container>
+      <Input onChange={handleSearchInput} />
+      <Button onClick={handleSearch}>api</Button>
+    </Container>
+  );
 };
 
 export default GoalCreate;
