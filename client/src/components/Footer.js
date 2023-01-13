b68da463b704bed7a03278bd6b72e62bd7d4e5cc
Donghwa <109756791+fairytale779@users.noreply.github.com>
2023-01-13 13:36:02 +0900

    [FE] donghwa 1.12 디자인 수정 (#19)
    * [FE Design] goalList 수정 중 시험 커밋
    
    * [FE Design] GoalList, Header 임시 수정
    
    * [FE] Footer 크기변경, 하단배치
    
    * [FE Design] 등록화면 디자인 구현
    
    * [FE fix] 푸터 위치값 수정
    
    * [FE Design] 상세 목표 틀 설정
    
    * [FE Design] 목표리스트 화면 구현 중
    
    Co-authored-by: 홍동화 <hhhhong44@gmail.com>

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,13 @@
 import styled from '@emotion/styled';
 
-const StyeldFooter = styled.footer``;
+const StyeldFooter = styled.footer`
+  width: 100%;
+  height: 200px;
+  background-color: #b1b2ff;
+  position: relative;
+  bottom: 0;
+  transform: translatY(-100%);
+`;
 
 const Footer = () => {
   return <StyeldFooter>푸터푸터푸터</StyeldFooter>;
--- client/src/pages/GoalCreate.js
+++ client/src/pages/GoalCreate.js
@@ -1,3 +1,55 @@
+
+import styled from '@emotion/styled';
+import { Button } from '@mui/material';
+// import { TextField } from '@mui/material';
+
+const GoalCreate = () => {
+  return (
+    <CreatePage>
+      <ComponentContain>
+        <br />
+        <div className="SettingLine">
+          <Header>나의 목표</Header>
+
+          <input
+            className="SettingInput"
+            placeholder="제네시스 GV80"
+            type="text"
+          />
+        </div>
+        <div className="SettingLine">
+          <Header>목표 금액</Header>
+          <input
+            className="SettingInput"
+            placeholder="61,360,000원"
+            type="number"
+          />
+          <Header style={{ color: '#b1b2ff' }}>원</Header>
+        </div>
+        <div className="SettingLine">
+          <Header>목표 기간</Header>
+          <input className="SettingInput" placeholder="72개월" type="number" />
+          <Header style={{ color: '#b1b2ff' }}>개월</Header>
+        </div>
+
+        <SavingCal>
+          <div style={{ display: 'flex' }}>
+            매달 &nbsp; <span style={{ color: 'red' }}>XX원</span> &nbsp;씩
+            모아야 해요!
+          </div>
+        </SavingCal>
+
+        <div className="buttonSet">
+          <Button
+            className="SubmitBox
+        "
+          >
+            <p>등록하기</p>
+          </Button>
+        </div>
+      </ComponentContain>
+    </CreatePage>
+
 import {
   Button,
   Container,
@@ -86,3 +138,85 @@ title: "웬디즈갤러리 조안나 워싱 프릴 원피스 FOP<b>123</b>"
 };
 
 export default GoalCreate;
+
+const CreatePage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ComponentContain = styled.div`
+  display: flex;
+  flex-direction: column;
+  display: inline-flex;
+  margin: 30px;
+  box-sizing: border-box;
+  width: 566px;
+  height: 278px;
+  background-color: #eef1ff;
+  border-radius: 6px;
+  .SettingLine {
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    margin-left: 20px;
+  }
+  .SettingInput {
+    box-sizing: border-box;
+    text-align: center;
+    background-color: transparent;
+    width: 400px;
+    height: 30px;
+    /* margin: px; */
+    font-size: 16px;
+    border: none;
+    border-radius: 6px;
+    border-bottom: solid 2px #b1b2ff;
+
+    margin-top: 20px;
+    color: grey;
+    &:focus {
+      outline: none;
+      border-color: #8ec3b0;
+      box-shadow: 0px 0px 0px 4px hsla(206, 100%, 40%, 0.15);
+    }
+  }
+  .buttonSet {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .SubmitBox {
+    box-sizing: border-box;
+    width: 214px;
+    height: 33px;
+    border-radius: 4px;
+    padding: 10px;
+    margin: 20px 0px 20px 0px;
+    text-align: center;
+    background-color: #b1b2ff;
+    :hover {
+      box-shadow: 0 5px 15px rgba(145, 92, 182, 0.4);
+      .li {
+        color: black;
+        font-weight: 700;
+      }
+    }
+  }
+`;
+
+// const SubmitBox = styled.div``;
+
+const Header = styled.h2`
+  margin: 30px 20px 0px 10px;
+`;
+
+const SavingCal = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  font-size: 16px;
+`;
