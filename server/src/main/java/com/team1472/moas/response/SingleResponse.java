4eeb832e76c3b908c92674b120f2f27ab6414ed0
ppk150 <vytpdnd11@naver.com>
2023-01-13 16:23:35 +0900

    [BE Feat] SingleResponse 클래스에 @AllArgsConstructor 어노테이션 추가 (생성자 자동생성)

--- server/src/main/java/com/team1472/moas/response/SingleResponse.java
+++ server/src/main/java/com/team1472/moas/response/SingleResponse.java
@@ -1,7 +1,9 @@
 package com.team1472.moas.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class SingleResponse<T> {
     private T data;
