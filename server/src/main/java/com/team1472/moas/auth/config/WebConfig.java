73d9a9b0a78789746e6c7938a16b1eabd97a8a47
ppk150 <vytpdnd11@naver.com>
2023-01-13 16:22:45 +0900

    [BE Feat] CORS에러 방지 WebConfig 추가

---
+++ server/src/main/java/com/team1472/moas/auth/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.team1472.moas.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// CORS 에러를 해결하기 위해 만들어 둔 부분, 사용 및 접근하고자 하는 링크를 등록하면 됨
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://ec2-43-201-0-232.ap-northeast-2.compute.amazonaws.com/")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization", "RefreshToken")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
