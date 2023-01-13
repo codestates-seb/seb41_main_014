38a9415e553628575580d3946c1a29db2a670fec
ppk150 <vytpdnd11@naver.com>
2023-01-13 16:41:18 +0900

    [BE Feat] MemberResponseDto추가, 회원정보 응답용 DTO 추가 (id, email, name, picture)

---
+++ server/src/main/java/com/team1472/moas/member/dto/MemberResponseDto.java
@@ -0,0 +1,25 @@
+package com.team1472.moas.member.dto;
+
+import com.team1472.moas.member.entity.Role;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MemberResponseDto {
+
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String picture;
+
+
+}
