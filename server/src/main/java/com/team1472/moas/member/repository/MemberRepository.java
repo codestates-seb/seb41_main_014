5fa04854010b2d517b98858bd8ffba287ae1b654
ppk150 <vytpdnd11@naver.com>
2023-01-13 16:44:16 +0900

    [BE Feat] MemberRepository 추가 / findByEmail, findByName, findByNameContaining, existsByName, existsByEmail  추가

---
+++ server/src/main/java/com/team1472/moas/member/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.team1472.moas.member.repository;
+
+import com.team1472.moas.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByName(String name);
+    List<Member> findByNameContaining(String name);
+    boolean existsByName(String name);
+    boolean existsByEmail(String email);
+}
