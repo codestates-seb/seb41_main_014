83f556511573e18ce6e47674d1b4bfd3bf40d539
GaHyeon <parka0017@gmail.com>
2023-01-16 10:09:43 +0900

    [BE Feat] 적금 리스트 필터링 조회 기능 구현 (#26)
    * [BE Build] queryDSL 의존성 추가
    
    * [BE Fix] PageInfo에 getter 추가
    
    * [BE Feat] 적금 상품 리스트 필터링 조회 기능 구현

---
+++ server/src/main/java/com/team1472/moas/installment_savings/controller/SavingProductsController.java
@@ -0,0 +1,42 @@
+package com.team1472.moas.installment_savings.controller;
+
+import com.team1472.moas.installment_savings.dto.SavingProductRes;
+import com.team1472.moas.installment_savings.dto.SavingsFilteringReq;
+import com.team1472.moas.installment_savings.service.SavingProductsService;
+import com.team1472.moas.response.MultiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/savings")
+@RequiredArgsConstructor
+@Validated
+public class SavingProductsController {
+    private final SavingProductsService savingProductsService;
+
+    /**
+     * 적금 정보 리스트 필터링 조회
+     */
+    @PostMapping
+    public ResponseEntity searchSavingProducts(@Positive @RequestParam("page") int page,
+                                               @Positive @RequestParam("size") int size,
+                                               @Valid @RequestBody SavingsFilteringReq savingsFilteringReq) {
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<SavingProductRes> pageSavingProducts = savingProductsService.findSavingsProducts(pageable, savingsFilteringReq);
+
+        List<SavingProductRes> savingProducts = pageSavingProducts.getContent();
+
+        return new ResponseEntity(new MultiResponse<>(savingProducts, pageSavingProducts), HttpStatus.OK);
+    }
+}
---
+++ server/src/main/java/com/team1472/moas/installment_savings/dto/SavingProductRes.java
@@ -0,0 +1,43 @@
+package com.team1472.moas.installment_savings.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SavingProductRes {
+    private long savingsId; //적금 상품 id
+
+    private long interestId; //금리 id
+
+    private String korCoNm; //금융회사 명
+
+    private String finPrdtNm; //금융상품명
+
+    private String joinWay; //가입 방법
+
+    private String spclCnd; //우대 조건
+
+    private String joinDeny; //가입제한
+
+    private String joinMember; //가입 대상
+
+    private String etcNote; //기타 유의사항
+
+    private int maxLimit; //최고 한도
+
+    private String intrRateTypeNm; // 저축 금리 유형명
+
+    private String rsrvTypeNm; //적립 유형명
+
+    private String saveTrm; // 저축 기간(단위: 개월)
+
+    private String mtrtInt; //만기 후 이자율
+
+    private double intrRate; //저축 금리(소수점 2자리)
+
+    private double intrRate2; //최고 우대 금리(소수점 2자리)
+
+}
