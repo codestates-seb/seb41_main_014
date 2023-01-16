05c68c94f4859e766b2ac23184072cbbbf5ec098
ppk150 <vytpdnd11@naver.com>
2023-01-16 16:49:30 +0900

    [BE MISC] 테스트용 야믈파일 넣은거 + api 인증키 넣음+ 테스트 일단 생략 -> 테스트 주석처리

--- server/src/test/java/com/team1472/moas/MoasApplicationTests.java
+++ server/src/test/java/com/team1472/moas/MoasApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MoasApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
