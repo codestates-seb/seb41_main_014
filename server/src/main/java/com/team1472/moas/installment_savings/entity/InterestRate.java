800e4a1b9457d758cb70dd2fe578376074680a5a
gahyeonn <parka0017@gmail.com>
2023-01-17 17:38:51 +0900

    [BE Refactor] 적금 관련 엔티티에서 불필요한 필드 삭제

--- server/src/main/java/com/team1472/moas/installment_savings/entity/InterestRate.java
+++ server/src/main/java/com/team1472/moas/installment_savings/entity/InterestRate.java
@@ -15,8 +15,6 @@ public class InterestRate {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String finCoNo; //금융 회사 코드
-
     private String finPrdtCd; //금융 상품 코드 => FK로 사용
 
     private String intrRateType; // 저축 금리 유형
@@ -32,13 +30,5 @@ public class InterestRate {
     private double intrRate; //저축 금리(소수점 2자리)
 
     private double intrRate2; //최고 우대 금리(소수점 2자리)
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "INSTALLMENTSAVINGS_ID")
-    private InstallmentSavings installmentSavings;
-
-    public void addInstallmentSavings(InstallmentSavings installmentSavings) {
-        this.installmentSavings = installmentSavings;
-    }
 
 }
