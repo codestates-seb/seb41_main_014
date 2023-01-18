f3d63659e13c9f6b007c2d4ec2c408c244918a56
gahyeonn <parka0017@gmail.com>
2023-01-18 16:52:47 +0900

    [BE Chore] 서버 자동 배포 위한 AWS CodeDeploy 설정

---
+++ server/scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+BUILD_JAR=$(ls /home/ubuntu/moas/server/build/libs/moas-0.0.1-SNAPSHOT.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+
+echo "> 현재 시간: $(date)" >> /home/ubuntu/moas/deploy.log
+
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/moas/deploy.log
+
+echo "> build 파일 복사" >> /home/ubuntu/moas/deploy.log
+DEPLOY_PATH=/home/ubuntu/moas/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/moas/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/moas/deploy.log
+else
+  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/moas/deploy.log
+  sudo kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
