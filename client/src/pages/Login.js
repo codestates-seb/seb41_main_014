9cc07bdf8b33c6feaac98d553c97bec22ae60e5f
iltae <iltae94@gmail.com>
2023-01-24 23:13:33 +0900

    [FE Feat] 로그인/멤버페이지 1차 화면 구성

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,34 @@
-import { styled } from '@mui/material/styles';
-// 아이콘 불러오는데 왜 자꾸 오류가 뜰까요...
-// import GoogleIcon from '@mui/icons-material/Google';
+import GoogleIcon from '@mui/icons-material/Google';
 import Button from '@mui/material/Button';
+import { Container, styled } from '@mui/material';
+
+const LoginContainer = styled(Container)`
+  background-color: ${(props) => props.theme.colors.mainLight};
+  height: 80vh;
+  margin-top: 8px;
+`;
 
 const GoogleLogin = styled(Button)`
-  padding: ${(props) => props.theme.spacing(2)};
-  margin: ${(props) => props.theme.spacing(1)};
+  margin-top: 37vh;
+  width: 100%;
+  height: 6vh;
+  div {
+    margin-left: 4px;
+  }
 `;
 
 const Login = () => {
   return (
     <>
-      <GoogleLogin href="http://ec2-43-201-0-232.ap-northeast-2.compute.amazonaws.com:8080/">
-        구글로 로그인하기
-      </GoogleLogin>
+      <LoginContainer>
+        <GoogleLogin
+          href="http://ec2-43-201-0-232.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/google"
+          variant="contained"
+        >
+          <GoogleIcon />
+          <div>구글로 로그인하기</div>
+        </GoogleLogin>
+      </LoginContainer>
     </>
   );
 };
