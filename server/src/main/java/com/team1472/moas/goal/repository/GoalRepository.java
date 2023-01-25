f28e35359c12bc2e4c6bcf9995dcd27bea8c0ad4
정유경 <83561356+YuKyung-Chung@users.noreply.github.com>
2023-01-25 23:08:33 +0900

    [BE MISC] 목표 도메인 api 호출에 jwt token 적용 (#68)

--- server/src/main/java/com/team1472/moas/goal/repository/GoalRepository.java
+++ server/src/main/java/com/team1472/moas/goal/repository/GoalRepository.java
@@ -1,10 +1,13 @@
 package com.team1472.moas.goal.repository;
 
+import com.team1472.moas.goal.dto.GoalRes;
 import com.team1472.moas.goal.entity.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GoalRepository extends JpaRepository<Goal, Long> {
+    Optional<Goal> findByIdAndMemberId(long Id, long memberId);
+    List<GoalRes> findGoalsByMemberId(long memberId);
 }
