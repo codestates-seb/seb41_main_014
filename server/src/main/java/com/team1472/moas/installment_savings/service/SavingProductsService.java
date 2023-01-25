b0b860edf6f7f1997c69c20d8efdbaea4f8c6f3b
gahyeonn <parka0017@gmail.com>
2023-01-25 18:02:55 +0900

    [BE MISC] 적금 필터링 조회 트랜잭션 추가

--- server/src/main/java/com/team1472/moas/installment_savings/service/SavingProductsService.java
+++ server/src/main/java/com/team1472/moas/installment_savings/service/SavingProductsService.java
@@ -8,11 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class SavingProductsService {
     private final InterestRateRepository interestRateRepository;
 
