96c9d0ba84fa56cccdb54be7098ebbb306f18855
YuKyung-Chung <kadie@naver.com>
2023-01-25 23:02:52 +0900

    [BE MISC] 목표 도메인 api 호출에 jwt token 적용

--- server/src/main/java/com/team1472/moas/goal/controller/GoalController.java
+++ server/src/main/java/com/team1472/moas/goal/controller/GoalController.java
@@ -3,9 +3,11 @@ package com.team1472.moas.goal.controller;
 
 import com.team1472.moas.goal.dto.GoalPatchReq;
 import com.team1472.moas.goal.dto.GoalPostReq;
+import com.team1472.moas.goal.dto.GoalRes;
 import com.team1472.moas.goal.entity.Goal;
 import com.team1472.moas.goal.mapper.GoalMapper;
 import com.team1472.moas.goal.service.GoalService;
+import com.team1472.moas.member.service.MemberService;
 import com.team1472.moas.response.MultiResponse;
 import com.team1472.moas.response.SingleResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -29,13 +32,27 @@ import java.util.List;
 @Tag(name = "Goals", description = "목표 API")
 public class GoalController {
     private final GoalService goalService;
+    private final MemberService memberService;
     private final GoalMapper mapper;
 
-    //목표 등록
+    /**
+     * 목표 등록
+     *
+     * @param principal
+     * @param goalPostReq
+     * @return ResponseEntity
+     */
     @Operation(summary = "목표 등록")
-    @PostMapping("/{member-id}")
-    public ResponseEntity postGoal(@PathVariable("member-id") @Positive long memberId,
+    @PostMapping()
+    public ResponseEntity postGoal(Principal principal,
                                    @Valid @RequestBody GoalPostReq goalPostReq) {
+        long memberId;
+        try {
+            memberId = memberService.findMemberId(principal.getName());
+        } catch (NullPointerException e) {
+            return new ResponseEntity<>("회원정보 없음", HttpStatus.OK);
+        }
+
         Goal createdGoal = goalService.createGoal(mapper.goalPostReqToGoal(goalPostReq), memberId);
 
         return new ResponseEntity<>(
@@ -43,48 +60,97 @@ public class GoalController {
                 HttpStatus.CREATED);
     }
 
-    //목표 수정
+    /**
+     * 목표 수정
+     *
+     * @param principal
+     * @param goalId
+     * @return ResponseEntity
+     */
     @Operation(summary = "목표 수정")
-    @PatchMapping("/{member-id}/{goal-id}")
-    public ResponseEntity patchGoal(@PathVariable("member-id") @Positive long memberId,
+    @PatchMapping("/{goal-id}")
+    public ResponseEntity patchGoal(Principal principal,
                                     @PathVariable("goal-id") @Positive long goalId,
                                     @Valid @RequestBody GoalPatchReq goalPatchReq) {
-                                    
-        Goal goal = goalService.updateGoal(mapper.goalPatchReqToGoal(goalPatchReq),goalId);
+        long memberId;
+        try {
+            memberId = memberService.findMemberId(principal.getName());
+        } catch (NullPointerException e) {
+            return new ResponseEntity<>("회원정보 없음", HttpStatus.OK);
+        }
+
+        Goal goal = goalService.updateGoal(mapper.goalPatchReqToGoal(goalPatchReq),goalId, memberId);
 
         return new ResponseEntity<>(
                 new SingleResponse<>(mapper.goalToGoalRes(goal)),
                 HttpStatus.OK);
     }
 
-    //목표 상세 조회
+    /**
+     * 목표 상세 조회
+     *
+     * @param principal
+     * @param goalId
+     * @return ResponseEntity
+     */
     @Operation(summary = "목표 상세 조회")
-    @GetMapping("/{member-id}/{goal-id}")
-    public ResponseEntity getGoal(@PathVariable("member-id") @Positive long memberId,
+    @GetMapping("/{goal-id}")
+    public ResponseEntity getGoal(Principal principal,
                                   @PathVariable("goal-id") @Positive long goalId) {
-        Goal goal = goalService.findGoal(goalId);
+        long memberId;
+        try {
+            memberId = memberService.findMemberId(principal.getName());
+        } catch (NullPointerException e) {
+            return new ResponseEntity<>("회원정보 없음", HttpStatus.OK);
+        }
+
+        Goal goal = goalService.findGoal(goalId, memberId);
         return new ResponseEntity<>(
                 new SingleResponse<>(mapper.goalToGoalRes(goal)),
                 HttpStatus.OK);
     }
 
-    //목표 전체 조회(페이지네이션 적용 X)
+    /**
+     * 목표 전체 조회(페이지네이션 적용 X)
+     *
+     * @param principal
+     * @return ResponseEntity
+     */
     @Operation(summary = "목표 전체 조회")
-    @GetMapping("/{member-id}")
-    public ResponseEntity getQuestions(@PathVariable("member-id") @Positive long memberId) {
-        List<Goal> goals = goalService.findGoals();
+    @GetMapping()
+    public ResponseEntity getQuestions(Principal principal) {
+        long memberId;
+        try {
+            memberId = memberService.findMemberId(principal.getName());
+        } catch (NullPointerException e) {
+            return new ResponseEntity<>("회원정보 없음", HttpStatus.OK);
+        }
+
+        List<GoalRes> goals = goalService.findGoals(memberId);
         return new ResponseEntity<>(
-                new MultiResponse<>(mapper.goalsToGoalRes(goals)),
+                new MultiResponse<>(goals),
                 HttpStatus.OK);
     }
 
-    //목표 삭제
+    /**
+     * 목표 삭제
+     *
+     * @param principal
+     * @param goalId
+     * @return ResponseEntity
+     */
     @Operation(summary = "목표 삭제")
-    @DeleteMapping("/{member-id}/{goal-id}")
-    public ResponseEntity deleteGoal(@PathVariable("member-id") @Positive long memberId,
+    @DeleteMapping("/{goal-id}")
+    public ResponseEntity deleteGoal(Principal principal,
                                      @PathVariable("goal-id") @Positive long goalId) {
-                                     
-        goalService.deleteGoal(goalId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        long memberId;
+        try {
+            memberId = memberService.findMemberId(principal.getName());
+        } catch (NullPointerException e) {
+            return new ResponseEntity<>("회원정보 없음", HttpStatus.OK);
+        }
+
+        goalService.deleteGoal(goalId, memberId);
+        return new ResponseEntity<>("성공적으로 삭제되었습니다.",HttpStatus.OK);
     }
 }
