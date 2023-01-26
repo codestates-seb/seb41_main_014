cb6fce2bfd66f3f265660593873c8d4714cddc52
iltae <106229016+iltae@users.noreply.github.com>
2023-01-26 13:55:16 +0900

    [Feat fix] 로그인/멤버 페이지 마무리 후 추가 기능 구현중 (#73)
    * [FE Feat] 로그인/멤버페이지 1차 화면 구성
    
    * [Feat fix] 로그인/멤버 페이지 마무리 후 추가 기능 구현중

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,22 @@
 import GoogleIcon from '@mui/icons-material/Google';
 import Button from '@mui/material/Button';
 import { Container, styled } from '@mui/material';
+import loginLogo from '../asset/images/login_2023.png';
 
 const LoginContainer = styled(Container)`
-  background-color: ${(props) => props.theme.colors.mainLight};
-  height: 80vh;
-  margin-top: 8px;
+  margin-top: 20vh;
+  height: 60vh;
+  text-align: center;
+  img {
+    width: 80%;
+  }
 `;
 
 const GoogleLogin = styled(Button)`
-  margin-top: 37vh;
-  width: 100%;
-  height: 6vh;
+  width: 55%;
+  height: 5vh;
+  color: ${(props) => props.theme.colors.white};
+  margin-top: 8px;
   div {
     margin-left: 4px;
   }
@@ -21,6 +26,7 @@ const Login = () => {
   return (
     <>
       <LoginContainer>
+        <img src={loginLogo} alt="로그인그림" />
         <GoogleLogin
           href="http://ec2-43-201-0-232.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/google"
           variant="contained"
