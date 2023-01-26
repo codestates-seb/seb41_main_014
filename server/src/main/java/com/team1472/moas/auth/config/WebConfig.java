38b19616ebd6e85b15a415be8a95c8550fd29cde
S2-CHICKEN <67466148+ppk150@users.noreply.github.com>
2023-01-26 18:14:40 +0900

    [BE FEAT] CORS 에러 해결용 http://gardensnake.kro.kr/ 추가 (#74)

--- server/src/main/java/com/team1472/moas/auth/config/WebConfig.java
+++ server/src/main/java/com/team1472/moas/auth/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://ec2-43-201-0-232.ap-northeast-2.compute.amazonaws.com/")
+                .allowedOrigins("http://localhost:3000","http://ec2-43-201-0-232.ap-northeast-2.compute.amazonaws.com/", "http://gardensnake.kro.kr/", "https://gardensnake.kro.kr/")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
